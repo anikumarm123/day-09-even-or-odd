1b57f6c526ef32e7f9f61f3237de4b6f94146c8b
muthukumar <anikumar2825@gmail.com>
2022-06-10 09:36:41 +0530

    add-file.

---
+++ script.js
@@ -0,0 +1,16 @@
+//n even numbers//
+// let n = 10;
+// for(i=0;i<=n;i++){
+//     if( i % 2 == 0){
+//         console.log("Even number"+i)
+//     }
+// }
+
+// Even or Odd//
+n = parseInt(prompt("Enter The Number"));
+if(n % 2 == 0){
+    console.log(n+ " is Even Number");
+}
+else{
+    console.log(n +" is Odd Number")
+}
